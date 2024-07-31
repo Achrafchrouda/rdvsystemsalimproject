b1da4d9a722ca1bbcbc041e31399574b13fe42a0
achrafchrouda <achrafchrouda@gmail.com>
2024-07-31 03:05:40 +0100

    Initial version of bookings generated by generator-jhipster@7.9.3

---
+++ bookings/src/test/java/com/satoripop/rdv/IntegrationTest.java
@@ -0,0 +1,23 @@
+package com.satoripop.rdv;
+
+import com.satoripop.rdv.BookingsApp;
+import com.satoripop.rdv.config.AsyncSyncConfiguration;
+import com.satoripop.rdv.config.EmbeddedSQL;
+import com.satoripop.rdv.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BookingsApp.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
