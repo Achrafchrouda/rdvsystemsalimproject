32e32efee4b7204938a7c0eac8bacff49216f082
achrafchrouda <achrafchrouda@gmail.com>
2024-07-31 03:03:57 +0100

    Initial version of accounts generated by generator-jhipster@7.9.3

---
+++ accounts/src/main/java/com/satoripop/rdv/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.satoripop.rdv.repository;
+
+import com.satoripop.rdv.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ accounts/src/main/java/com/satoripop/rdv/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.satoripop.rdv.service.dto;
