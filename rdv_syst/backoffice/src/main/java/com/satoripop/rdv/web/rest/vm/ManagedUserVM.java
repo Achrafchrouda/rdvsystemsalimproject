90ec01c19cee5b85723a423423c19f1c6ae66d4e
achrafchrouda <achrafchrouda@gmail.com>
2024-07-31 03:29:14 +0100

    Initial version of backoffice generated by generator-jhipster@7.9.3

---
+++ rdv_syst/backoffice/src/main/java/com/satoripop/rdv/web/rest/vm/ManagedUserVM.java
@@ -0,0 +1,19 @@
+package com.satoripop.rdv.web.rest.vm;
+
+import com.satoripop.rdv.service.dto.AdminUserDTO;
+
+/**
+ * View Model extending the AdminUserDTO, which is meant to be used in the user management UI.
+ */
+public class ManagedUserVM extends AdminUserDTO {
+
+    public ManagedUserVM() {
+        // Empty constructor needed for Jackson.
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "ManagedUserVM{" + super.toString() + "} ";
+    }
+}
